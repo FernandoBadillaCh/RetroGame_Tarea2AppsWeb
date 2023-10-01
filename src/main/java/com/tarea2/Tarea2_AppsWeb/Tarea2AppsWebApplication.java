c92a9b2670d4e375ae1344cc4f42501c2ba0ab37
Fernando Badilla Chinchilla <133604779+FernandoBadillaCh@users.noreply.github.com>
2023-09-30 20:15:19 -0600

    Creacion de Doc

---
+++ src/main/java/com/tarea2/Tarea2_AppsWeb/Tarea2AppsWebApplication.java
@@ -0,0 +1,13 @@
+package com.tarea2.Tarea2_AppsWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2AppsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2AppsWebApplication.class, args);
+	}
+
+}
